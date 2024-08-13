739aad53ef69b208275b2a1ae2e820f8f6c2d7f3
BrainX Shopify <brainxshopify@BrainXs-MacBook-Pro.local>
2024-08-13 19:04:46 +0500

    overflow issue resolved, magnify js, mobile cards,videos

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,18 +1,28 @@
+
 function displayImage(imageId) {
     const imageSrc = document.getElementById(imageId).src;
     const displayDiv = document.getElementById('image-selected');
     displayDiv.innerHTML = `<img class="zoom-img" src="${imageSrc}" alt="Displayed Image">`;
-}
-    window.onload = function() {
-    displayImage('image6');
+
+    const img = displayDiv.querySelector('img');
+    let x = 1.5; 
+
+    img.addEventListener("mousemove", (e) => {
+        img.classList.add("magnify");
+        let posX = e.offsetX / img.width * 100;
+        let posY = e.offsetY / img.height * 100;
+        
+        img.style.transformOrigin = `${posX}% ${posY}%`;
+        img.style.transform = `scale(${x})`;
+    });
+
+    img.addEventListener("mouseout", () => {
+        img.classList.remove("magnify");
+        img.style.transform = "none";
+    });
 }
 
-// var options={
-//     width:400,
-//     height:200,
-//     zoomWidth:500,
-//     offset:{vertical:0, horizontal:0},
-//     scale:1.5
-// }
+window.onload = function() {
+    displayImage('image6'); 
+}
 
-// new ImageZoom(document.getElementById('image-selected') , options);
