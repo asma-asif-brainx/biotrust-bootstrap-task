e04c8dd3cfb03c8ed41e75bf5dc3fd05343edc5f
BrainX Shopify <brainxshopify@BrainXs-MacBook-Pro.local>
2024-08-15 19:08:13 +0500

    vertical corousal,watch button video, css changes, responsivness

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -26,7 +26,41 @@ window.onload = function() {
     displayImage('image6'); 
 }
 
+function displayVideo() {
+    const displayDiv = document.getElementById('image-selected');
+    displayDiv.innerHTML = `
+        <iframe class="video-slider" id="video1" src="https://player.vimeo.com/video/551517271" alt="Video" 
+        onclick="displayVideo('video1')" style="cursor: pointer;" 
+        frameborder="0"></iframe>`;
+}
+
+function activateVideoCarousel() {
+    const carousel = document.querySelector('#veritical-demo-medium');
+    const carouselInner = carousel.querySelector('.carousel-inner');
+    const videoItems = carouselInner.querySelectorAll('.carousel-item iframe');
+
+    if (videoItems.length > 0) {
+        carouselInner.querySelectorAll('.carousel-item').forEach(item => {
+            item.classList.remove('active');
+        });
+
+        const firstVideoItem = videoItems[0].closest('.carousel-item');
+        firstVideoItem.classList.add('active');
+
+        const bootstrapCarousel = new bootstrap.Carousel(carousel);
+        const videoItemIndex = Array.from(carouselInner.children).indexOf(firstVideoItem);
+        bootstrapCarousel.to(videoItemIndex);
+    } else {
+        console.error("No video items found in the carousel.");
+    }
+}
 
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  });
